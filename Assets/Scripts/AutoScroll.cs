c060b558f9d495ea0ad1e4195ce1f9d50ba1f32b
20191197음영준 <eimuc00@naver.com>
2023-08-05 00:23:05 +0900

    1차 업로드
    - 오토스크롤 기능
    - 점프 기능
    - 공중 대쉬기능

---
+++ Assets/Scripts/AutoScroll.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public enum ResetOn { ON, OFF }
+
+[System.Serializable]
+public class LoopSetting
+{
+    public Transform resetPos;
+    public float maxCameraToDistance;
+}
+
+public class AutoScroll : MonoBehaviour
+{
+    public ResetOn isLoop;
+    private Transform mainCamera;
+
+    Vector2 scrollPosition;
+    public float speed;
+
+    public LoopSetting loopSetting;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main.GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        if (isLoop == ResetOn.ON)
+            StartCoroutine(LoopObject());
+
+        scrollPosition = transform.position;
+    }
+
+    void Update()
+    {
+        scrollPosition.x -= Time.deltaTime * speed;
+        transform.position = scrollPosition;
+    }
+
+    private IEnumerator LoopObject()
+    {
+        while (true)
+        {
+            if (Mathf.Abs(mainCamera.position.x - transform.position.x) > loopSetting.maxCameraToDistance)
+            {
+                scrollPosition.x = loopSetting.resetPos.position.x;
+                transform.position = scrollPosition;
+            }
+            yield return null;
+        }
+    }
+}
